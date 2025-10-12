f2848a257ef90f835364b2ebc9c550a32ce01d39
solacite <julia.nguyen.do@gmail.com>
2025-10-11 22:07:24 -0400

    successfully implemented semi functional player mvmt

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,39 +1,43 @@
 extends CharacterBody3D
 
-const SPEED = 8.0
-const JUMP_VELOCITY = 9.0
-var mouse_sens := 0.002
+@export var speed := 8.0
+@export var jump_velocity := 9.0
+@export var mouse_sens := 0.002
 var pitch := 0.0
 var tgt_pitch := 0.0
-const PITCH_UP := deg_to_rad(80) # up lim
-const PITCH_DN := deg_to_rad(-30) # dn lim
+const PITCH_UP := deg_to_rad(80)
+const PITCH_DN := deg_to_rad(-30)
 
 var pivot
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) # lock mouse
-	pivot = $Pivot # get pivot
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	pivot = $Pivot
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		rotate_y(-event.relative.x * mouse_sens) # horiz rot
-		tgt_pitch = clamp(tgt_pitch - event.relative.y * mouse_sens, PITCH_DN, PITCH_UP) # vert rot lim
+		rotate_y(-event.relative.x * mouse_sens)
+		tgt_pitch = clamp(tgt_pitch - event.relative.y * mouse_sens, PITCH_DN, PITCH_UP)
 
 func _process(delta):
-	pitch = lerp(pitch, tgt_pitch, 8.0 * delta) # smooth pitch
-	pivot.rotation.x = pitch # set pitch
+	pitch = lerp(pitch, tgt_pitch, 8.0 * delta)
+	pivot.rotation.x = pitch
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta):
+	var inp_dir = Input.get_vector("move_left", "move_right", "move_fwd", "move_bkwd")
+	var direction = Vector3.ZERO
+	# In Godot, forward/backward is Z, left/right is X
+	direction.x = inp_dir.x
+	direction.z = inp_dir.y
+	direction = (transform.basis * direction).normalized()
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+
+	# Gravity & Jump
 	if not is_on_floor():
-		velocity.y -= ProjectSettings.get_setting("physics/3d/default_gravity") * delta # gravity
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY # jump
-	var inp_dir = Input.get_vector("move_left", "move_right", "move_fwd", "move_bkwd") # get dir
-	var dir = (transform.basis * Vector3(inp_dir.x, 0, inp_dir.y)).normalized() # local dir
-	if dir != Vector3.ZERO:
-		velocity.x = dir.x * SPEED # x vel
-		velocity.z = dir.z * SPEED # z vel
+		velocity.y -= ProjectSettings.get_setting("physics/3d/default_gravity") * delta
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED) # x decel
-		velocity.z = move_toward(velocity.z, 0, SPEED) # z decel
-	move_and_slide() # move
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = jump_velocity
+
+	move_and_slide()
