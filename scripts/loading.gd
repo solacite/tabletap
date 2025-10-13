45b3217dcf8f7d0b47c376da7c1974fa5b4ea4ca
solacite <julia.nguyen.do@gmail.com>
2025-10-12 23:29:22 -0400

    fix loading scene script + autoloaded

---
+++ scripts/loading.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var LoadingScreen = preload("res://scenes/loading_screen.tscn")
+var loading_screen_instance = null
+
+func show_loading_screen():
+	if loading_screen_instance == null:
+		loading_screen_instance = LoadingScreen.instantiate()
+		get_tree().current_scene.add_child(loading_screen_instance)
+
+func hide_loading_screen():
+	if loading_screen_instance:
+		loading_screen_instance.queue_free()
+		loading_screen_instance = null
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,4 +1,22 @@
 extends Node2D
 
+var LoadingScreen = preload("res://scenes/loading_screen.tscn")
+
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	transition_to_scene("res://scenes/main.tscn")
+
+func transition_to_scene(target_scene_path: String):
+	var loading_screen = show_loading_screen()
+	ResourceLoader.load_threaded_request(target_scene_path)
+	while ResourceLoader.load_threaded_get_status(target_scene_path) == ResourceLoader.THREAD_LOAD_IN_PROGRESS:
+		await get_tree().process_frame
+	if ResourceLoader.load_threaded_get_status(target_scene_path) == ResourceLoader.THREAD_LOAD_LOADED:
+		var next_scene = ResourceLoader.load_threaded_get(target_scene_path)
+		get_tree().change_scene_to_packed(next_scene)
+	elif ResourceLoader.load_threaded_get_status(target_scene_path) == ResourceLoader.THREAD_LOAD_FAILED:
+		print("Failed to load scene: %s" % target_scene_path)
+
+func show_loading_screen():
+	var loading_screen = LoadingScreen.instantiate()
+	get_tree().current_scene.add_child(loading_screen)
+	return loading_screen
