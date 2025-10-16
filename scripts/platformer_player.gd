f6aab7b2d83d3228e50c1e535fbb477880ea3588
solacite <julia.nguyen.do@gmail.com>
2025-10-15 22:40:41 -0400

    fix player jump mvmt, fix camera, fix platformer setup overall

--- scripts/platformer_player.gd
+++ scripts/platformer_player.gd
@@ -1,21 +1,11 @@
 extends CharacterBody2D
 
 # constants
-const SPEED = 125.0
-const DASH_SPEED = 300.0
-const DASH_DURATION = 0.2
-const JUMP_VELOCITY = 275.0
-const CLIMB_SPEED = 100.0
+const SPEED = 250.0
+const JUMP_VELOCITY = 400.0
 
 var respawn_position : Vector2
 
-var is_dashing = false
-var dash_timer = 0.0
-
-var is_kill: bool = false
-
-# ladder sys
-var on_ladder: bool = false
 @onready var tilemap
 
 # anim & visuals
@@ -93,28 +83,19 @@ func _physics_process(delta: float) -> void:
 func handle_normal_movement(delta: float):
 	# apply gravity
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-	
+		velocity.y += gravity * delta
+
 	# jump input
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	
-	# dash input
-	if Input.is_action_just_pressed("dash") and not is_dashing:
-		start_dash()
-		
-	if is_dashing:
-		dash_timer -= delta
-		if dash_timer <= 0:
-			is_dashing = false
-	
+		velocity.y = -JUMP_VELOCITY
+
 	# horizontal input
 	var direction = 0
 	if Input.is_action_pressed("move_left"):
 		direction -= 1
 	if Input.is_action_pressed("move_right"):
 		direction += 1
-	
+
 	# set horizontal vel
 	if direction:
 		velocity.x = direction * SPEED
@@ -124,28 +105,9 @@ func handle_normal_movement(delta: float):
 func update_animations():
 	var animated_sprite = $AnimatedSprite2D
 	
-	# climbing anim
-	if on_ladder and (Input.is_action_pressed("move_up") or Input.is_action_pressed("move_down")):
-		animated_sprite.play("idle")
-		return
-	
-	# jump/fall
-	if not is_on_floor() and not on_ladder:
-		if velocity.y < 0:
-			animated_sprite.play("jump")
-		else:
-			animated_sprite.play("fall")
-	
-	elif velocity.x != 0:
+	if velocity.x != 0:
 		animated_sprite.play("run")
 		animated_sprite.flip_h = velocity.x < 0
 		
 	else:
 		animated_sprite.play("idle")
-
-func start_dash():
-	is_dashing = true
-	dash_timer = DASH_DURATION
-	var dash_direction = 1 if not $AnimatedSprite2D.flip_h else -1
-	velocity.x = dash_direction * DASH_SPEED
-	velocity.y = 0
