02d8e11a58f80e3b24ff9ff90804d0c6c06b138b
solacite <julia.nguyen.do@gmail.com>
2025-10-22 21:28:18 -0400

    remove shader from loading screen because it's bugging ..

--- scripts/transition.gd
+++ scripts/transition.gd
@@ -1,22 +1,23 @@
 extends ColorRect
 
-# show anim shader overlay
-func show_overlay():
-	var mat = self.material
-	if mat == null:
-		print("No ShaderMaterial assigned to this ColorRect!")
-		return
-		
-	# shader params
-	mat.set_shader_parameter("in_out", 0.0)
-	
-	# anim
-	var tween = create_tween()
-	tween.tween_property(mat, "shader_parameter/in_out", 1.0, 1.0)
-	
-	# set colors
-	mat.set_shader_parameter("in_color", Color.html("#eebf83")) # peach
-	mat.set_shader_parameter("out_color", Color(0, 0, 0, 1)) # black
+var tween_position: float = -0.2
+var tween_in_out: float = 0.0
 
 func _ready():
-	show_overlay()
+	if material != null:
+		material = material.duplicate()
+	material.set_shader_parameter("in_color", Color.html("#eebf83"))
+	material.set_shader_parameter("out_color", Color(0, 0, 0, 1))
+	material.set_shader_parameter("size", Vector2(8, 8))
+	in_transition(2.0)
+
+func _process(delta):
+	material.set_shader_parameter("position", tween_position)
+	material.set_shader_parameter("in_out", tween_in_out)
+
+func in_transition(duration: float):
+	tween_position = -0.2
+	tween_in_out = 0.0
+	var tween = create_tween()
+	tween.tween_property(self, "tween_position", 1.0, duration)
+	tween.tween_property(self, "tween_in_out", 1.0, duration)
